518aaff5589079b3d6b42671468a79792854af2a
metabirdsfdc <aquasensethefuture@gmail.com>
2025-12-26 21:57:40 +0530

    Initial commit for my Portfolio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-yellow-400">
+      <p className="text-5xl font-bold">Pavan Kalyan's Portfolio</p>
+    </div>
+  );
+};
+
+export default App;
